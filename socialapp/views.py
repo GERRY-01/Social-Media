0d2cb49834b5e15a66c7bda64c6aef859d4a5a5b
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-02 14:12:18 +0300

    added redirect option to redirect the page after submission

--- socialapp/views.py
+++ socialapp/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from . models import Posts
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        file = request.FILES.get("media")
+        caption = request.POST.get("caption")
+        my_posts = Posts(media = file, caption = caption)
+        my_posts.save()
+        return redirect("home")
+    
+    all_posts = Posts.objects.all().order_by('-id')
+    return render(request, 'home.html', {'all_posts':all_posts})
