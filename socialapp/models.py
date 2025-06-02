d6cbc99fc5e9e66086b906405a2946b748ef86d8
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-02 17:52:57 +0300

    added the model for comments

--- socialapp/models.py
+++ socialapp/models.py
@@ -5,3 +5,9 @@ class Posts(models.Model):
     media = models.FileField(upload_to='files')
     caption = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+class Comments(models.Model):
+    post = models.ForeignKey(Posts, on_delete = models.CASCADE, related_name="comments")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
