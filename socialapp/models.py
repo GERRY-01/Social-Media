d5b61d34bae4d300088911ffbf898d145c694d4a
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-02 14:09:40 +0300

    added a feature to make the caption to be optional

--- socialapp/models.py
+++ socialapp/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Posts(models.Model):
     media = models.FileField(upload_to='files')
-    caption = models.TextField()
+    caption = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
