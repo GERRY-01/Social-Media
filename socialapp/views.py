633aff211a74d4f8bf84cccadd873389f1bdbd5a
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-02 17:56:32 +0300

    added get_object_or_404 to display error 404 not found incase someone navigates to a url with the id that doesn't exists

--- socialapp/views.py
+++ socialapp/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render,redirect
-from . models import Posts
+from django.shortcuts import get_object_or_404, render,redirect
+from . models import Comments, Posts
 # Create your views here.
 def home(request):
     if request.method == 'POST':
-        file = request.FILES.get("media")
-        caption = request.POST.get("caption")
-        my_posts = Posts(media = file, caption = caption)
-        my_posts.save()
-        return redirect("home")
+        if 'media' in request.FILES:
+            file = request.FILES.get("media")
+            caption = request.POST.get("caption")
+            my_posts = Posts(media = file, caption = caption)
+            my_posts.save()
+            return redirect("home")
     
     all_posts = Posts.objects.all().order_by('-id')
     return render(request, 'home.html', {'all_posts':all_posts})
+
+def add_comment(request, post_id):
+    if request.method == 'POST':
+        comment_text = request.POST.get("comment_text")
+        post = get_object_or_404(Posts, id=post_id)
+        Comments.objects.create(post=post, content=comment_text)
+        return redirect("home")
