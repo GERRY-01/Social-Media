3e114b2a69ba85d6c6d108578ba1c93d56bcbc53
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-03 12:38:16 +0300

    added the login

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import login,authenticate
+from .models import Registration
 from django.contrib import messages
 # Create your views here.
 
@@ -28,7 +30,36 @@ def register(request):
         if has_error:
             return redirect("register")
         
-        user_data = User(first_name = first_name,last_name = last_name, username = username, email = email, password=password)
+        user_data = User.objects.create_user(first_name = first_name,last_name = last_name, username = username, email = email, password=password)
         user_data.save()
+        login(request, user_data)
         return redirect("completeregistration")
     return render(request, 'registration.html')
+
+def completeregistration(request):
+    if request.method == 'POST':
+        user = request.user
+        profile = request.FILES.get('profile_picture')
+        dob = request.POST.get("dob")
+        gender = request.POST.get("gender")
+        location = request.POST.get("location")
+        hobbies = request.POST.get("hobbies")
+        
+        reg_data = Registration(user = user, image = profile, dob = dob, gender = gender, location = location, hobbies = hobbies)
+        reg_data.save()
+        return redirect("home")
+    return render(request, 'completeregistration.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username = username, password = password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, 'invalid login')
+        
+    return render(request, 'login.html')
