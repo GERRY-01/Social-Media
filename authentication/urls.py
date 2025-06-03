ea1d17e08bd9c39741e7141861c305909ebfa762
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-03 12:39:31 +0300

    added the path for logging in

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path("/register",views.register,name="register"),
     path("/register/complete_registration",views.completeregistration,name="completeregistration"),
+    path("/login", views.user_login, name="login")
     
 ]
