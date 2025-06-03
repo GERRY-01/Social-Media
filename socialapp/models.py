34d00254aa5f810dbef764214ca9254800df334e
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-03 21:28:39 +0300

    added the model for likes

--- socialapp/models.py
+++ socialapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
 class Posts(models.Model):
@@ -7,7 +8,15 @@ class Posts(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     
 class Comments(models.Model):
+    #user = models.ForeignKey(User, on_delete= models.CASCADE)
     post = models.ForeignKey(Posts, on_delete = models.CASCADE, related_name="comments")
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Likes(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    post = models.ForeignKey(Posts, on_delete = models.CASCADE, related_name="likes")
+    created_at = models.DateTimeField(auto_now_add=True)
     
+    class Meta:
+        unique_together = ('user','post')
