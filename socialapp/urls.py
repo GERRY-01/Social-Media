7b731c483f41d93a327266fb12c302a82515af38
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-03 21:25:50 +0300

    added a path for liking the posts

--- socialapp/urls.py
+++ socialapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('/home', views.home, name="home"),
     path('add_comment/<int:post_id>/', views.add_comment, name='add_comment'),
+    path('likes/<int:post_id>', views.likes, name="likes")
 ]
