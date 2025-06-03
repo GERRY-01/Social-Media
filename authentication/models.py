7d0b9ddcc75b2fbb12d2516dfab0d13bb585cad1
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-03 11:20:13 +0300

    added the model for complete registration

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Registration(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='pics')
+    dob = models.DateField()
+    gender_choices = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other'),   
+    ]
+    
+    gender = models.CharField(choices=gender_choices, max_length=10)
+    location = models.CharField(max_length=50)
+    hobbies = models.CharField(max_length=200)
