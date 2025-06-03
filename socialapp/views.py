739c181a81282d1924bb50c65934fabee1cbab27
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-03 12:02:50 +0300

    displaying the profile picture and the name of the user if authenticated

--- socialapp/views.py
+++ socialapp/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import get_object_or_404, render,redirect
 from . models import Comments, Posts
+from authentication.models import Registration,User
 # Create your views here.
 def home(request):
+    if request.user.is_authenticated: 
+        try:
+            profile = Registration.objects.get(user = request.user)
+        except Registration.DoesNotExist:
+            profile = None
+            
+    else: 
+        profile = None
     if request.method == 'POST':
         if 'media' in request.FILES:
             file = request.FILES.get("media")
@@ -11,7 +20,7 @@ def home(request):
             return redirect("home")
     
     all_posts = Posts.objects.all().order_by('-id')
-    return render(request, 'home.html', {'all_posts':all_posts})
+    return render(request, 'home.html', {'all_posts':all_posts,'profile':profile})
 
 def add_comment(request, post_id):
     if request.method == 'POST':
