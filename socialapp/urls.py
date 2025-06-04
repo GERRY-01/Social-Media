97694b6623a2cc4f943669ca8020356c3f48b55e
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-04 07:49:22 +0300

    added the url for directing to the profile

--- socialapp/urls.py
+++ socialapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('/home', views.home, name="home"),
     path('add_comment/<int:post_id>/', views.add_comment, name='add_comment'),
-    path('likes/<int:post_id>', views.likes, name="likes")
+    path('likes/<int:post_id>', views.likes, name="likes"),
+    path('/profile',views.profile,name="profile"),
 ]
