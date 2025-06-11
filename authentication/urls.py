96d51b03d63ee74dc3f84efe53b3bb2b7f121299
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-11 16:02:33 +0300

    added the url for logout

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
     path("/register",views.register,name="register"),
     path("/register/complete_registration",views.completeregistration,name="completeregistration"),
-    path("/login", views.user_login, name="login")
+    path("/login", views.user_login, name="login"),
+    path("/logout", views.user_logout, name="logout"),
     
 ]
