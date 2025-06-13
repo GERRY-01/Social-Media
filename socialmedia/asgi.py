b6eb048c355ef11b7df9a10d8c6a9068f2d4fa0b
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-13 15:41:23 +0300

    made some changes in the asgi file

--- socialmedia/asgi.py
+++ socialmedia/asgi.py
@@ -6,11 +6,24 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
-
+import django
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialmedia.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from authentication.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialmedia.settings')
-
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        )
+    )
+})
