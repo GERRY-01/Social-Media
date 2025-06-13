d21e4098e1b95b66d6e456d4ccdc2070471b2d83
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-13 15:40:22 +0300

    added the timestamp for showing the time a message was sent

--- authentication/consumers.py
+++ authentication/consumers.py
@@ -3,6 +3,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from .models import Room, Message,Registration
 from django.contrib.auth.models import User
+from django.utils.timezone import now,localtime
 
 def get_room_name(sender_id, receiver_id):
     sorted_ids = sorted([sender_id, receiver_id])
@@ -33,28 +34,33 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = data['message']
         sender = self.scope["user"]
         receiver = await database_sync_to_async(User.objects.get)(id=self.other_user_id)
-        await database_sync_to_async(Message.objects.create)(
+        message_obj = await database_sync_to_async(Message.objects.create)(
             room=self.room,
             sender=sender,
             receiver=receiver,
-            message=message
+            message=message,
+            sent_at= localtime(now())
         )
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message,
-                'sender': sender.username,
-                'receiver': receiver.username
+                'message': message_obj.message,
+                'sender': message_obj.sender.username,
+                'receiver':message_obj.receiver.username,
+                'sent_at': message_obj.sent_at.isoformat()
             }
         )
-        
+
     async def chat_message(self, event):
         message = event['message']
         sender = event['sender']
         receiver = event['receiver']
+        sent_at = event['sent_at']
         await self.send(text_data=json.dumps({
             'message': message,
             'sender': sender,
-            'receiver': receiver
+            'receiver': receiver,
+            'sent_at': sent_at
         }))
+        
